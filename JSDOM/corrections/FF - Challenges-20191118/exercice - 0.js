91a86cbf9d5ed7aa0cf2c68707f0537744bb870c
Frederic Lamagnere <fre.lamagnere@gmail.com>
2020-03-03 14:24:14 +0100

    Add JSDOM

---
+++ JSDOM/corrections/FF - Challenges-20191118/exercice - 0.js
@@ -0,0 +1,14 @@
+/*
+	L'objectif de cet exercice est de cr�er une
+	animation dans un balise canvas. Il s'agit
+	d'animer un lever - couch� de soleil sur une
+	plaine.
+
+	- Cr��z une balise canvas;
+	- Cr�ez un dessin qui repr�sente le ciel;
+	- Cr�er un dessin qui repr�sente le soleil;
+	- Cr�ez un dessin qui repr�sente votre plaine;
+	- Le soleil doit d�crire un arc de cercle au dessus de la plaine;
+	- La luminosit� ambiante doit varier en fonction de la hauteur
+	  du soleil dans l'espace de dessin.
+*/	
